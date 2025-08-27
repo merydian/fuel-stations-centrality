6823020bdf6ac0d83939d087e932cc3bc6f3a8ec
Till Frankenbach <till.frankenbach@gmail.com>
2025-08-27 15:17:50 +0200

    Update SAMPLE_NODES in config: increase number of nodes to sample from 10,000 to 50,000

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
     CLUSTER_RADIUS = 5000  # meters - stations within this radius are combined
 
     # Graph sampling parameter
-    SAMPLE_NODES = 10000  # Number of nodes to sample from road network (None = no sampling)
+    SAMPLE_NODES = 50000  # Number of nodes to sample from road network (None = no sampling)
 
     # Data limits
     MIN_STATIONS_REQUIRED = 10
