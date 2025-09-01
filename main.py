6f2bfc0cca80dd273cc627d89593c3a9bf7617a7
Till Frankenbach <till.frankenbach@gmail.com>
2025-09-01 14:06:05 +0200

    Refactor: Organize main function by adding comments for each pruning step and ensure assertions are correctly placed; add logging for exporting igraph edges to GeoPackage

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def main():
     Config.ensure_directories()
     Config.validate_config()
 
+    # prepare network and stations
     road_filepath = Config.get_road_filepath()
     G_road_nx = ox.load_graphml(road_filepath)
     G_road_nx.remove_edges_from(nx.selfloop_edges(G_road_nx))
@@ -43,26 +44,27 @@ def main():
     name_to_idx = {v["name"]: v.index for v in G_road_ig.vs}
     stations_ig = [name_to_idx[str(nx_node)] for nx_node in stations_nx if str(nx_node) in name_to_idx]
 
-
-    assert len(stations_ig) == len(stations_nx)
-    
-    stations_knn_ig = nodes_highest_avg_knn_distance_ig(G_road_ig, knn=Config.K_NN, n=Config.N_REMOVE, node_subset=stations_ig)
-
+    # 1. Prune base network
     G_road_ig = prune_igraph_by_distance(G_road_ig, stations_ig, Config.MAX_DISTANCE)
 
+    # 2. Prune base network based on knn stations
+    stations_knn_ig = nodes_highest_avg_knn_distance_ig(G_road_ig, knn=Config.K_NN, n=Config.N_REMOVE, node_subset=stations_ig)
     remaining_stations_knn_ig = set(stations_ig) - set(stations_knn_ig)
     G_road_filtered_ig = prune_igraph_by_distance(G_road_ig.copy(), remaining_stations_knn_ig, Config.MAX_DISTANCE)
 
+    # 3. Prune base network based on random stations
     random.seed(Config.RANDOM_SEED)
     random_stations_ig = random.sample(stations_ig, Config.N_REMOVE)
-    assert random_stations_ig != stations_knn_ig
-
     remaining_stations_random_ig = set(stations_ig) - set(random_stations_ig)
     G_road_random_ig = prune_igraph_by_distance(G_road_ig.copy(), remaining_stations_random_ig, Config.MAX_DISTANCE)
-
+    
+    # Assertions to check validity of analysis
+    assert len(stations_ig) == len(stations_nx)
+    assert random_stations_ig != stations_knn_ig
     assert G_road_random_ig is not None
     assert G_road_filtered_ig is not None
 
+    # Save the things for visualization
     logger.info(f"Exporting igraph edges to GeoPackage...")
     igraph_edges_to_gpkg(G_road_random_ig, "random")
     igraph_edges_to_gpkg(G_road_filtered_ig, "knn")
